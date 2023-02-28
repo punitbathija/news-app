046388e298a77cf87014c701dd4a21c834692063
Punit Bathija <punit644@gmail.com>
2023-02-28 13:25:12 +0530

    Added a header

--- app/Header.js
+++ app/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 bg-blak text-white">
+      <h3>New's Ninja</h3>
+    </div>
+  );
 }
 
 export default Header;
