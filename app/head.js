4d0f8e9f3edbd4173e3da4f8d5739e393d6b32c4
Punit Bathija <punit644@gmail.com>
2023-02-28 13:05:53 +0530

    Initiated the project with Tailwind and latest Next

---
+++ app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>New's Ninja</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Stay ahead of the game" />
+      <link rel="icon" href="/favicon-32x32.png" />
+    </>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Header from "./header";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+    </>
+  );
+}
