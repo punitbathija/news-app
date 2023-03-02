abac902986229ed931551b03784a16a40da8fff2
Punit Bathija <punit644@gmail.com>
2023-03-02 15:08:21 +0530

    Adding finishing touches

---
+++ .history/app/category/page_20230302150538.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { BiArrowBack, BiCategory, BiCodeBlock } from "react-icons/bi";
+import { GiNewspaper } from "react-icons/gi";
+import { IoMdBusiness } from "react-icons/io";
+import { MdLocalMovies, MdSportsCricket } from "react-icons/md";
+import { news } from "../../public/newspaper.svg";
+import Link from "next/link";
+
+function Category() {
+  return (
+    <div className="flex flex-col gap-5 text-center justify-center justify-items-center px-16 py-16 font-bold">
+      <h2 className="flex gap-3 text-center justify-center justify-items-center px-16 py-16 font-bold text-2xl">
+       { if(
+        <Link href="/" />)
+        <Link href="/" className="hidden">
+          <BiArrowBack />
+        </Link>
+    }
+
+        Browse News By Category <BiCategory />
+      </h2>
+      <div className="flex flex-col justify-between mx-24 gap-3 md:flex-row">
+        <div className="card w-96 bg-neutral text-neutral-content">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">
+              Gerneral
+              <GiNewspaper />
+            </h2>
+            <Link href="/category/general">
+              <p>Browse Gerneral News.</p>
+            </Link>
+          </div>
+        </div>
+        <div className="card w-96 bg-neutral text-neutral-content">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">
+              Business
+              <IoMdBusiness />
+            </h2>
+            <Link href="/category/business">
+              <p>Browse Business News.</p>
+            </Link>
+          </div>
+        </div>
+        <div className="card w-96 bg-neutral text-neutral-content">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">
+              Sports
+              <MdSportsCricket />
+            </h2>
+            <Link href="/category/sports">
+              <p>Browse Sports News.</p>
+            </Link>
+          </div>
+        </div>
+        <div className="card w-96 bg-neutral text-neutral-content">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">
+              Technology
+              <BiCodeBlock />
+            </h2>
+            <Link href="/category/technology">
+              <p>Browse Technology News.</p>
+            </Link>
+          </div>
+        </div>
+        <div className="card w-96 bg-neutral text-neutral-content">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">
+              Entertainment
+              <MdLocalMovies />
+            </h2>
+            <Link href="/category/entertainment">
+              <p>Browse Entertainment News.</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
